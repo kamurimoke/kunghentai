76e4cc6589b65ec9d31dcccb09b8307eed28073e
kamurimoke <31116368+kamurimoke@users.noreply.github.com>
2017-08-18 15:14:54 +0700

    Update api.php

--- api.php
+++ api.php
@@ -44,7 +44,7 @@ if (strpos($_msg, 'เพิ่มความหื่น') !== false) {
     $arrPostData = array();
     $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
     $arrPostData['messages'][0]['type'] = "text";
-    $arrPostData['messages'][0]['text'] = 'ขอบคุณที่สอนบทเรียนอันล้ำค่า';
+    $arrPostData['messages'][0]['text'] = 'ขอบคุณที่สอนบทเรียนอันล้ำค่า!!!';
   }
 }else{
   if($isData >0){
